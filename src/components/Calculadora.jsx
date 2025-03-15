c38998250db126d2cae54dad680c4fd524567aea
Mudufin-184 <velandiafonseca@gmail.com>
2025-03-15 08:20:00 -0500

    ya funciona la calculadora solo que no renderiza el resultado

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -1,63 +1,47 @@
-import { Component } from "react";
+import { useState } from "react";
 
-class Calculadora extends Component {
-    constructor(props) {
-        super(props);
+const Calculadora = (props) => {
+    
+    const [state,setState] = useState({
+        numero1: 0,
+        numero2: 0
+    })
 
-        this.state = {
-            numero1: 0,
-            numero2: 0
-        };
-
-        this.setNumber1 = this.setNumber1.bind(this);
-        this.setNumber2 = this.setNumber2.bind(this);
+    const setNumber1 = (e) => {  
+       setState({
+        ...state,
+        numero1: e.target.value
+       })
     }
 
-    setNumber1(e) {  
-        this.setState({ numero1: Number(e.target.value) });
+    const setNumber2 = (e) => {
+       setState({
+        ...state,
+        numero2: e.target.value
+       })
     }
-
-    setNumber2(e) {
-        this.setState({ numero2: Number(e.target.value) });
-    }
-
-    render() {
-        return (
-            <>
-                <form>
-                    <h2>{this.props.nombre}</h2>
-                    <div>
-                        <input 
-                            type="number" 
-                            name="numero1" 
-                            value={this.state.numero1}
-                            onChange={this.setNumber1} 
-                        />
-
-                        <input 
-                            type="number" 
-                            name="numero2" 
-                            value={this.state.numero2}
-                            onChange={this.setNumber2}
-                        />
-                    </div>
-                </form>
-                
-                <span> Resultado: {this.state.numero1 + this.state.numero2} </span>
-            </>
-        );
-    }
-
-
-    //tercer paso del ciclo de vida
-    componentDidMount(){
-
-    }
-
-    //Cuarto paso del ciclo de vida 
-    componentWillUnmount(){
-
-    }
+  
+    return (
+        <>
+        <h2>{props.nombre}</h2> 
+            <form>                    
+                <input 
+                    type="number" 
+                    name="numero1" 
+                    value={state.numero1}
+                    onChange={setNumber1} 
+                />
+                <input 
+                    type="number" 
+                    name="numero2" 
+                    value={state.numero2}
+                    onChange={setNumber2}
+                />           
+            </form>
+            
+            <span> Resultado: {Number(state.numero1) + Number(state.numero2)} </span>
+        </>
+    );
 }
 
 export default Calculadora;
